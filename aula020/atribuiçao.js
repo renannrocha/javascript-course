548acc50e7cb07de8bc5d361a467a047f6713c29
renannrocha <renanrocha8897@gmail.com>
2022-12-12 14:11:51 -0300

    exercicios

--- aula020/atribuiçao.js
+++ aula020/atribuiçao.js
@@ -1,1 +1,29 @@
 // OPERADORES DE ATRIBUIÇÃO
+
+// variavel operador = valor ;
+
+let contador = 2;
+contador *= 2;
+console.log(contador);
+
+
+//##########################################################################################################################################
+
+// convertando strings em numeros.
+
+// 1° forma - tranformando string em numero inteiro
+
+let exemplonum1 = parseInt("5"); 
+console.log(typeof exemplonum1) // number
+
+// 2° forma - transformando string em numero real 
+
+let exemplonum2 = parseFloat("5.7");
+console.log(typeof exemplonum2) // number
+
+// 3° forma - transformando string em tipo number 
+
+let exemplonum3 = Number("345");
+console.log(typeof exemplonum3) // number
+
+//##########################################################################################################################################
