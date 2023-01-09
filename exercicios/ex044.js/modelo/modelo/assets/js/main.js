29a912c17fff783f8034c5fe08971972770f9a5b
renannrocha <renanrocha8897@gmail.com>
2023-01-09 18:55:40 -0300

    pratica

--- exercicios/ex044.js/modelo/modelo/assets/js/main.js
+++ exercicios/ex044.js/modelo/modelo/assets/js/main.js
@@ -1,4 +1,13 @@
 function meuEscopo(){
+    const form = document.querySelector('#formulario');
+    const resultado = document.querySelector('.resultado');
 
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+        const peso = form.querySelector('#peso');
+        const altura = form.querySelector('#altura');
+    }
+    
+    form.addEventListener('submit', recebeEventoForm);
 }
-meuEscopo()
+meuEscopo();
